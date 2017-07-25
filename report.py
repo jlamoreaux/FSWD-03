d9b8f17e90a3401e0d6893208224a28af36489fa
Jorcan Lamoreaux <jnacious88@gmail.com>
2017-07-24 19:45:04 -0500

    Initial commit

---
+++ report.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+import psycopg2
+import sys
+
+conn = psycopg2.connect("dbname=news")
+
+cursor = conn.cursor()
+
+def popular_articles()
+    cursor.execute(
+
+    )
+
+def popular_authors()
+    cursor.execute(
+
+    )
+
+def high_errors()
+    cursor.execute(
+    
+    )
