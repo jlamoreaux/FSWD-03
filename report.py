8a2c8d6d087c11c69d54b09bc504e93244cd6ff0
Jorcan Lamoreaux <jnacious88@gmail.com>
2017-07-27 22:21:22 -0500

    adjust formatting to match pep8 style guide

--- report.py
+++ report.py
@@ -1,31 +1,41 @@
 #!/usr/bin/python
+
 import psycopg2
-import sys
+
 
 conn = psycopg2.connect("dbname=news")
 
 cursor = conn.cursor()
 
+
+# List of top 3 popular articles
 def popular_articles():
     cursor.execute(
         SELECT articles.title, COUNT(log.ip)
         FROM articles
-        JOIN log ON log.path LIKE CONCAT('%', articles.slug, '%')
-        GROUP BY title ORDER BY COUNT DESC LIMIT 3;
+        JOIN log ON log.path
+        LIKE CONCAT('%', articles.slug, '%')
+        GROUP BY title
+        ORDER BY COUNT DESC
+        LIMIT 3;
     )
-    return
 
+
+# List of authors in order of popularity
 def popular_authors():
     cursor.execute(
         SELECT authors.name, COUNT(popular.ip)
         FROM authors
-        JOIN (SELECT articles.title, articles.author, log.ip
-        FROM articles
-        JOIN log ON log.path LIKE CONCAT('%', articles.slug, '%')) AS popular
-        ON authors.id = popular.author
-        GROUP BY name ORDER BY COUNT DESC;
+        JOIN(SELECT articles.title, articles.author, log.ip FROM articles
+             JOIN log ON log.path LIKE CONCAT('%', articles.slug, '%'))
+        AS popular
+        ON authors.id=popular.author
+        GROUP BY name
+        ORDER BY COUNT DESC;
     )
 
+
+# Day with the highest number of errors
 def high_errors():
     cursor.execute(
         SELECT DATE(time) AS ForDate, COUNT(status)
@@ -34,3 +44,7 @@ def high_errors():
         ORDER BY COUNT DESC
         LIMIT 1;
     )
+
+popular_articles()
+popular_authors()
+high_errors()
