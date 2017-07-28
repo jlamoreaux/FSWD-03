affbfc159da1d1d1ded1f70bcbeafb5178f87aad
Jorcan Lamoreaux <jnacious88@gmail.com>
2017-07-27 21:31:00 -0500

    complete second and third objectives

--- report.py
+++ report.py
@@ -6,7 +6,7 @@ conn = psycopg2.connect("dbname=news")
 
 cursor = conn.cursor()
 
-def popular_articles()
+def popular_articles():
     cursor.execute(
         SELECT articles.title, COUNT(log.ip)
         FROM articles
@@ -15,12 +15,22 @@ def popular_articles()
     )
     return
 
-def popular_authors()
+def popular_authors():
     cursor.execute(
-
+        SELECT authors.name, COUNT(popular.ip)
+        FROM authors
+        JOIN (SELECT articles.title, articles.author, log.ip
+        FROM articles
+        JOIN log ON log.path LIKE CONCAT('%', articles.slug, '%')) AS popular
+        ON authors.id = popular.author
+        GROUP BY name ORDER BY COUNT DESC;
     )
 
-def high_errors()
+def high_errors():
     cursor.execute(
-
+        SELECT DATE(time) AS ForDate, COUNT(status)
+        FROM log WHERE status != '200 OK'
+        GROUP BY ForDate
+        ORDER BY COUNT DESC
+        LIMIT 1;
     )
